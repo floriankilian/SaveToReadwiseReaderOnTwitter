9df2cf5432b8b365958808228f8abca705df632a
floriankilian <34104015+floriankilian@users.noreply.github.com>
2024-02-07 23:22:52 +0100

    improve readability

--- SaveToReadwiseReaderFromTwitter.user.js
+++ SaveToReadwiseReaderFromTwitter.user.js
@@ -13,19 +13,20 @@
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
 // @license      MIT
-
 // ==/UserScript==
 
 (function () {
     'use strict';
 
+// Constants
     const baseUrl = 'https://twitter.com';
-
     const defaultSVG = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-clipboard" viewBox="0 0 24 24" stroke-width="2" stroke="#71767C" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" /></svg>';
     const copiedSVG = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-clipboard-check" viewBox="0 0 24 24" stroke-width="2" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" /><path d="M9 14l2 2l4 -4" /></svg>';
     const savedtoReaderSVG = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-reader-check" viewBox="0 0 24 24" stroke-width="2" stroke="#FDE704" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" /><path d="M9 14l2 2l4 -4" /></svg>';
 
+// Prompt for API key if not set
     const apiKey = promptForApiKey();
+
     // Function to prompt for API key if not set
     function promptForApiKey() {
         let apiKey = GM_getValue("apiKey");
@@ -36,9 +37,9 @@
         return apiKey;
     }
 
+// Add copy button to tweets
     function addCopyButtonToTweets() {
         const tweets = document.querySelectorAll('article[data-testid="tweet"]');
-
         tweets.forEach(tweet => {
             if (!tweet.querySelector('.custom-copy-icon')) {
                 const copyIcon = document.createElement('span');
@@ -60,36 +61,32 @@
                             .catch(err => console.error('Error copying link: ', err));
                     }
                 });
-
                 tweet.appendChild(copyIcon);
             }
         });
     }
 
+// Extract tweet URL from tweet element
     function extractTweetUrl(tweetElement) {
         const linkElement = tweetElement.querySelector('a[href*="/status/"]');
-
         if (!linkElement) {
             return;
         }
-
         let url = linkElement.getAttribute('href').split('?')[0]; // Remove any query parameters
-
         if (url.includes('/photo/')) {
             url = url.split('/photo/')[0];
         }
         return `${baseUrl}${url}`;
     }
 
+// Save tweet URL to Readwise
     function saveTweetUrlToReadwise(tweetUrl, copyIcon) {
-        const apiToken = apiKey; // Use this if you want to set your API token manually (not very safe) 'YOUR_API_TOKEN'; // Replace this with your actual Readwise token
-        const readerApiUrl = 'https://readwise.io/api/v3/save/'; // Adjust API URL if needed
-
+        const apiToken = apiKey;
+        const readerApiUrl = 'https://readwise.io/api/v3/save/';
         const data = {
             url: tweetUrl,
-            category: 'tweet' // Adjust based on Readwise's expected categories, if needed
+            category: 'tweet'
         };
-
         GM_xmlhttpRequest({
             method: "POST",
             url: readerApiUrl,
@@ -113,18 +110,15 @@
         });
     }
 
-
+// Adjust icon style based on tweet type
     function adjustIconStyle(tweet, copyIcon) {
-        // Find all spans within the tweet
-        const spans = tweet.querySelectorAll('span');
+                const spans = tweet.querySelectorAll('span');
         let isIndividualTweet = false;
-
         spans.forEach(span => {
             if (span.textContent.includes("Views")) {
                 isIndividualTweet = true;
             }
         });
-
         if (isIndividualTweet) {
             copyIcon.style.cssText = 'width: 22px; height: 22px; cursor: pointer; padding: 2px 5px; margin: 2px; position: absolute; bottom: 9px; right: 64px';
         } else {
@@ -132,9 +126,10 @@
         }
     }
 
-
+// Observe mutations and add copy button to new tweets
     const observer = new MutationObserver(addCopyButtonToTweets);
     observer.observe(document.body, { childList: true, subtree: true });
 
+// Add copy button to existing tweets
     addCopyButtonToTweets();
 })();
